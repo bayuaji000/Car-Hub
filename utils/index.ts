e55c0fb448839aa5609a51a57c1b76990d2e2a1c
bayuaji000 <bayuaji.2999@gmail.com>
2025-05-07 03:25:55 +0700

    api update

--- utils/index.ts
+++ utils/index.ts
@@ -52,7 +52,7 @@ export async function fetchCars(filters: FilterProps) {
 
   // Set the required headers for the API request
   const response = await fetch(
-    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=toyota&model=corolla&year=2020&limit=10&fuel_type=gas`,
     {
       headers: headers,
     }
